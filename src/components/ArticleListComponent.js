edec3f15d7bde16f11667de2cfc56ea57a1cbb17
viren <vsr266@nyu.edu>
2019-07-20 00:06:54 -0400

    added css

--- src/components/ArticleListComponent.js
+++ src/components/ArticleListComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const ArticleListComponent = ({ articles }) => (
     <>
         {articles.map((article, key) => (
-            <Link to={`/article/${article.name}`} key={key}>
+            <Link className= 'article-list-item' to={`/article/${article.name}`} key={key}>
                 <h3> {article.title} </h3>
                 <p> {article.content.substring(0, 50)}...</p>
             </Link>
